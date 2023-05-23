7fc299109ac3e7203d540a143f5143a6a5635bb3
Crowdbotics <team@crowdbotics.com>
2023-05-23 16:20:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-waterfall-40928",
+  url: "https://bold-waterfall-40928.botics.co",
+  api: "https://bold-waterfall-40928.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
